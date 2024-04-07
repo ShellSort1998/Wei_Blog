4218cde2fbe9bbcb6255c5ab5f0316291f456c21
root <ShellSort1998@github.com>
2024-04-07 15:37:23 +0800

    server's code

--- src/main/java/com/blog/weiblog/config/Constants.java
+++ src/main/java/com/blog/weiblog/config/Constants.java
@@ -2,5 +2,5 @@ package com.blog.weiblog.config;
 
 public class Constants {
     //文件上传路径 /opt/deploy/upload/
-    public final static String FILE_UPLOAD_DIC = "D:\\PostgraduateLearning\\CAREER_DERICTION\\java-dev\\Wei-Blog\\src\\main\\resources\\upload\\";
+    public final static String FILE_UPLOAD_DIC = "/opt/deploy/upload/";
 }
